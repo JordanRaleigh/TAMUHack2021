6730e4fdc5e03d74dbab27b16c6ac167603d5214
Saketh Rudraraju <svrudy1@gmail.com>
2021-01-31 03:01:50 -0600

    notworking-imagescanner

--- src/AuthRoute.js
+++ src/AuthRoute.js
@@ -1,16 +1,19 @@
 //make a react funtonal component
 // use routes to redirect from react router dom
-import React from 'react';
-import { Route, Redirect, Component } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect, Component } from "react-router-dom";
+import Client from "./Client";
 
 function AuthRoute({ component: Component, ...rest }) {
-  let isAuthenticated = false;
-
   return (
     <Route
       {...rest}
       render={(props) =>
-        isAuthenticated ? <Component {...props} /> : <Redirect to="/signin" />
+        Client.isLoggedIn() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/signin" />
+        )
       }
     />
   );
