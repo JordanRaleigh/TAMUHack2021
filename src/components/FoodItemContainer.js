161a52084034c2b7bf9920081eeae42693c1ac40
Jordan Raleigh <jraleigh1127@gmail.com>
2021-01-30 20:45:34 -0600

    added downvote upvote

--- src/components/FoodItemContainer.js
+++ src/components/FoodItemContainer.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const FoodItem = (props) => {
+  //add use state to show if its upvoted
+  const [votes, setVotes] = useState(0);
+  const handleSumbit = () => {
+    //api logic stufff
+    console.log(votes);
+  };
   return (
     <div className="">
       <div>Name : {props.name}</div>
       <div>Restaurant : {props.restaurant}</div>
       <div>Upvotes : {props.upvotes}</div>
       <div>Downvotes : {props.downvotes}</div>
+      <button
+        onClick={() => {
+          setVotes(1);
+          handleSumbit();
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setVotes(-1);
+          handleSumbit();
+        }}
+      >
+        -
+      </button>
     </div>
   );
 };
